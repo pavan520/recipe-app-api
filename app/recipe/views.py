a2ed323c378cb335ef57686b19b775eb63485829
pvan520 <kantamani.pavan520@gmail.com>
2020-04-25 11:56:43 +0530

    tag app setup with models,serializer,urls

---
+++ app/recipe/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from app.core.models import Tag
+from app.recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin, mixins.CreateModelMixin):
+    """ Manage tags in the DB"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """ Return objects for current authenticated user only"""
+        return self.queryset.filter(user= self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """ Create a new Tag"""
+        serializer.save(self.request.user)
